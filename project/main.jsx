dd36a61f2cf5c3c037d13ab3136e82ba0520d01d
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-11-23 20:50:04 -0600

    init commit

---
+++ project/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { App } from './src/App.jsx'
+const root = createRoot(document.getElementById('app'))
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
