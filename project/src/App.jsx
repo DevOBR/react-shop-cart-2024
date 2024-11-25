bc7196e35ad6e22aa483bf7822ae13f94d4f418d
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-11-24 21:03:27 -0600

    Adding functionality to add, remove and clear product in cart.

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useId, useState } from 'react'
 import { products as productsInitiate } from './mocks/products.json'
-import { CartIcon, ClearCartIcon, AddToCartIcon } from './components/Icons.jsx'
+import {
+  CartIcon,
+  ClearCartIcon,
+  AddToCartIcon,
+  RemoveFromCartIcon
+} from './components/Icons.jsx'
 import './App.css'
 
 export function App() {
@@ -10,6 +15,75 @@ export function App() {
   const [minPrice, setMinPrice] = useState(0)
   const [category, setCategory] = useState('all')
   const [productList, setProductList] = useState()
+  const [productCartList, setProductCartList] = useState([])
+
+  const productAction = (p) => {
+    const isInCart = productCartList?.some((prod) => prod.id === p.id)
+    if (isInCart) {
+      return (
+        <>
+          <button onClick={() => handleAddProdctToCart(p)}>
+            <AddToCartIcon />
+          </button>
+          <button onClick={() => handleRemoveProdctToCart(p)}>
+            <RemoveFromCartIcon />
+          </button>
+        </>
+      )
+    } else {
+      return (
+        <button onClick={() => handleAddProdctToCart(p)}>
+          <AddToCartIcon />
+        </button>
+      )
+    }
+  }
+
+  const handleRemoveProdctToCart = (product) => {
+    const productToRemoveIndex = productCartList.findIndex(
+      (p) => p.id === product.id
+    )
+
+    const newProductCartList = structuredClone(productCartList)
+
+    if (
+      productToRemoveIndex >= 0 &&
+      productCartList[productToRemoveIndex].quantity > 0
+    ) {
+      newProductCartList[productToRemoveIndex].quantity--
+      if (newProductCartList[productToRemoveIndex].quantity === 0) {
+        setProductCartList(
+          newProductCartList.filter(
+            (p) => p.id !== newProductCartList[productToRemoveIndex].id
+          )
+        )
+      } else {
+        setProductCartList(newProductCartList)
+      }
+    }
+  }
+
+  const handleAddProdctToCart = (product) => {
+    const productToAddIndex = productCartList.findIndex(
+      (p) => p.id === product.id
+    )
+
+    const newProductCartList = structuredClone(productCartList)
+    const productIndex = productList.findIndex((p) => p.id === product.id)
+
+    if (
+      productToAddIndex >= 0 &&
+      productCartList[productToAddIndex].quantity <
+        productList[productIndex].quantity
+    ) {
+      newProductCartList[productToAddIndex].quantity++
+      setProductCartList(newProductCartList)
+    } else if (productToAddIndex === -1) {
+      // if is nww then add it
+      setProductCartList([...newProductCartList, { ...product, quantity: 1 }])
+    }
+  }
+
   const handleMinPrice = (e) => {
     setMinPrice(e.target.value)
   }
@@ -62,18 +136,15 @@ export function App() {
         {productList &&
           productList.map((p) => (
             <ul key={p.id}>
-              <li>{p.title}</li>
+              <li>
+                {p.title} - Quantity {p.quantity}
+              </li>
               <li>$ {p.price}</li>
               <li>
                 <img src={p.thumbnail} alt={p.description} />
               </li>
               <li>{p.description}</li>
-              <li className='prod-action-button'>
-                <p>Quantity - {p.quantity}</p>
-                <button>
-                  <AddToCartIcon />
-                </button>
-              </li>
+              <li className='prod-action-button'>{productAction(p)}</li>
             </ul>
           ))}
       </section>
@@ -85,8 +156,8 @@ export function App() {
       <input type='checkbox' id={cartCheckboxId} hidden />
       <section className='cart'>
         <ul>
-          {productList &&
-            productList.map((p) => (
+          {productCartList &&
+            productCartList.map((p) => (
               <li key={p.id}>
                 <img src={p.thumbnail} alt={p.title} />
                 <div>
@@ -94,13 +165,13 @@ export function App() {
                 </div>
 
                 <footer>
-                  <small>Qty: 1</small>
-                  <button>+</button>
+                  <small>Quantity: {p.quantity}</small>
+                  <button onClick={() => handleAddProdctToCart(p)}>+</button>
                 </footer>
               </li>
             ))}
         </ul>
-        <button className='clear-list'>
+        <button className='clear-list' onClick={() => setProductCartList([])}>
           <ClearCartIcon />
         </button>
       </section>
