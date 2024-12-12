b50e947466f95e4ba74b362fa7fdf6c1c55d7ffe
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-12-11 20:20:26 -0600

    Save the cart to localStorage so it can be recovered when the page reloads

--- project/src/contexts/cart.context.jsx
+++ project/src/contexts/cart.context.jsx
@@ -1,12 +1,13 @@
 import { createContext, useReducer } from 'react'
 import { useFilters } from '../hooks/useFilters.hook'
 import { cartReducer } from '../reducers/cart.reducer'
+const storedCarList = JSON.parse(window.sessionStorage.getItem('shop-car'))
 
 export const CartContext = createContext()
 
 export function CartProvider({ children }) {
   const { filteredProducts } = useFilters()
-  const [productCartList, dispatch] = useReducer(cartReducer, [])
+  const [productCartList, dispatch] = useReducer(cartReducer, storedCarList)
 
   const handler = (action, product) => {
     dispatch({
--- project/src/reducers/cart.reducer.js
+++ project/src/reducers/cart.reducer.js
@@ -3,7 +3,7 @@ import { CART_ACTIONS } from '../consts/const'
 const actions = {
   [CART_ACTIONS.ADD]: (state, filteredProducts, product) => {
     const productToAddIndex = state.findIndex((p) => p.id === product.id)
-    const newProductCartList = structuredClone(state)
+    let newProductCartList = structuredClone(state)
     const productIndex = filteredProducts.findIndex((p) => p.id === product.id)
     if (
       productToAddIndex >= 0 &&
@@ -11,15 +11,20 @@ const actions = {
         filteredProducts[productIndex].quantity
     ) {
       newProductCartList[productToAddIndex].quantity++
-      return newProductCartList
     } else if (productToAddIndex === -1) {
       // if is nww then add it
-      return [...newProductCartList, { ...product, quantity: 1 }]
-    } else return newProductCartList
+      newProductCartList = [...newProductCartList, { ...product, quantity: 1 }]
+    }
+
+    window.sessionStorage.setItem(
+      'shop-car',
+      JSON.stringify(newProductCartList)
+    )
+    return newProductCartList
   },
   [CART_ACTIONS.REMOVE]: (state, filteredProducts, product) => {
     const productToRemoveIndex = state.findIndex((p) => p.id === product.id)
-    const newProductCartList = structuredClone(state)
+    let newProductCartList = structuredClone(state)
 
     if (
       productToRemoveIndex >= 0 &&
@@ -27,15 +32,21 @@ const actions = {
     ) {
       newProductCartList[productToRemoveIndex].quantity--
       if (newProductCartList[productToRemoveIndex].quantity === 0) {
-        return newProductCartList.filter(
+        newProductCartList = newProductCartList.filter(
           (p) => p.id !== newProductCartList[productToRemoveIndex].id
         )
       }
     }
-
+    window.sessionStorage.setItem(
+      'shop-car',
+      JSON.stringify(newProductCartList)
+    )
     return newProductCartList
   },
-  [CART_ACTIONS.CLEAR]: () => []
+  [CART_ACTIONS.CLEAR]: () => {
+    window.sessionStorage.removeItem('shop-car')
+    return []
+  }
 }
 
 export function cartReducer(state, req) {
